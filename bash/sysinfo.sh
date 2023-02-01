424ec66c79d26ac3a8f31a43dc37662a53f713d8
Sushil Koirala <koirala7sushil@gmail.com>
2023-02-01 18:44:29 -0500

    Assignment done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo  "FQDN:$(hostname) "
-echo  "Host Information: "
-hostnamectl status
-echo  "IP Address:$(hostname -I) $(ip addr | grep -w inet6 | awk '{print $2}' | grep -v ::1/128
-)"
+echo  "FQDN:$(hostname) "  #echo command prints or displays the value of variables.
+echo  "Host Information: " # host information refers to the name assigned to a computer on a network, which is used to identify the machine.
+hostnamectl status #hostnamectl used to manage the system hostname.
+echo  "IP Address:$(hostname -I)"
 echo  "Root Filesystem Status: "
-df -h /boot/efi
+df -h /boot/efi # df -h commands produces output that lists all files that are currently mounted on system, along with information about the total size, used and available space. 
