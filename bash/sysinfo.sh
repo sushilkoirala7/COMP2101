4667ea279f88415830e500a15f934227e1e46375
Sushil Koirala <koirala7sushil@gmail.com>
2023-02-15 20:31:25 -0500

    Assignment done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-hostname=$(hostname)
-OS_Name=$(lsb_release -d | grep Description | cut -f2)
-IP=$(ip route | grep -w default | cut -d' ' -f3)
-Free_Space=$(df -h / | awk 'NR==2 {print $4}')
+hostname=$(hostname)        #The hostname of the current system can be obtained with the command $(hostname).
+OS_Name=$(lsb_release -d | grep Description | cut -f2)  #This command is used to obatin description of the current operating system distribution.
+IP=$(ip route | grep -w default | cut -d' ' -f3)  # This command helps to get the ip address of the default gateway.
+Free_Space=$(df -h / | awk 'NR==2 {print $4}')   #The root partition's available disc space can be obtained with this command.
 
 cat<<EOF
 
